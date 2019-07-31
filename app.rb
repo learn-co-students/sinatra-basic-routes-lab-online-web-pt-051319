fbc579c21a3e00c302fb5e5357c2c822352fded2
Jamie Vullo <jvullo@protonmail.com>
2019-07-31 16:40:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Jamie"
+  end
+  
+  get '/hometown' do
+    "My hometown is Taylor, PA"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is ?"
+  end
+  
 end
